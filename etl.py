d580709268dea49bb99cdce89adf556f23b8712f
crazymooner <crazymooner@gmail.com>
2014-08-04 22:10:12 -0700

    Add backFill for etl.py
    Signed-off-by: crazymooner <crazymooner@gmail.com>

--- etl.py
+++ etl.py
@@ -7,15 +7,20 @@ from optparse import OptionParser
 from pi.mysql.client import mysqlConnection
 from csv import DictReader
 from pi.ibHelper.barfeed import RealTimeBar
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 import traceback
+import copy
 
 def get_options():
     parser = OptionParser()
+    parser.add_option('-s', '--startdate', dest='startdate', action='store',
+                      help='start date')
+    parser.add_option('-e', '--enddate', dest='enddate', action='store',
+                      help='end date')
     parser.add_option('-f', '--filepath', dest='filepath', action='store',
-        help='file directory')
+                      help='file directory')
     parser.add_option('-l', '--log', dest='logFilename', action='store',
-        help='logFilename')
+                      help='logFilename')
     parser.add_option('-t', '--datetime', dest='datetime', action='store',
                       default=(datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d"),
                       help='datetime')
@@ -38,14 +43,17 @@ def dailyEtl(options):
         if dailyETLSanityCheck(options):
             if options.logFilename is not None:
                 f = open(options.logFilename, 'a')
+                f.write("\n********************************************\n")
                 f.write("dailyETL finished " + options.datetime + "\n")
     except Exception:
         traceback.print_exc()
         if options.logFilename is not None:
             f = open(options.logFilename, 'a')
+            f.write("\n********************************************\n")
             f.write("dailyETL failed " + options.datetime + "\n")
 
 def dailyETLSanityCheck(options):
+    #symbol count(*) 
     return True
 
 def dailyViewETLSanityCheck(options):
@@ -71,7 +79,19 @@ def dailyViewEtl(options):
 
 if __name__ == '__main__':
     options = get_options()
-    if options.dailyETL:
-        dailyEtl(options)
-    if options.dailyViewETL:
-        dailyViewEtl(options)
+    if options.startdate is not None and options.enddate is not None:
+        start = datetime.strptime(options.startdate, "%Y-%m-%d")
+        end = datetime.strptime(options.enddate, "%Y-%m-%d")
+        while start <= end:
+            optionCopy = copy.deepcopy(options)
+            optionCopy.datetime = start.strftime("%Y-%m-%d")
+            start = start + timedelta(days=1)
+            if options.dailyETL:
+                dailyEtl(optionCopy)
+            if options.dailyViewETL:
+                dailyViewEtl(optionCopy)
+    else:
+        if options.dailyETL:
+            dailyEtl(options)
+        if options.dailyViewETL:
+            dailyViewEtl(options)
